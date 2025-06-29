c9078f405ffcb3fb16d08f96e6c1943603d11ad1
azlinaaylin <azlinaarabi@gmail.com>
2025-06-29 13:29:04 +0600

    works code changes because of responsive

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -3,16 +3,16 @@ import WorksbackDesign from "../assets/images/WorksDesign.png";
 
 const Works = () => {
   return (
-    <div className="px-12 py-12 bg-transparent">
-      <div className="px-12 py-50 bg-gray-100 relative">
-        <img src={WorksbackDesign} />
+    <div className="px-2 py-6 bg-transparent sm:px-6 md:px-12 md:py-12">
+      <div className="px-2 py-6 bg-gray-100 relative sm:px-6 md:px-12 md:py-12">
+        <img src={WorksbackDesign} className="w-full object-cover" />
 
-        <div className="ml-5 mr-5 bg-transparent rounded-r-md absolute top-[5%] left-[25%]">
-          <div className="flex flex-col flex-wrap justify-center items-center mt-8">
-            <p className="font-lato font-bold text-[48px] leading-[58px] mb-4">
+        <div className="mx-2 bg-transparent rounded-r-md relative lg:absolute lg:top-[5%] lg:left-[25%]">
+          <div className="flex flex-col flex-wrap justify-center items-center mt-4 md:mt-8">
+            <p className="font-lato font-bold text-[28px] leading-[36px] mb-2 md:text-[48px] md:leading-[58px] md:mb-4">
               HOW IT WORKS
             </p>
-            <p className="font-main font-normal text-[16px] leading-[27px] w-[651px] text-center">
+            <p className="font-main font-normal text-[14px] leading-[22px] w-full text-center md:text-[16px] md:leading-[27px] md:w-[651px]">
               There are many variations of passages of Lorem Ipsum available,
               but the majority have suffered alteration in some.
             </p>
@@ -20,46 +20,45 @@ const Works = () => {
         </div>
 
         {/* card_1 */}
-        <div className="bg-transparent px-4 py-8 mr-5  ml-5 absolute top-[18%] left-[7%]">
-          <div className="bg-white w-[426px] h-[286px] rounded-xl flex flex-wrap flex-col justify-center items-center">
-            <div className="w-[70px] h-[70px] bg-gray-200 rounded-full relative">
-              <div className="w-[45px] h-[45px] rounded-full bg-black absolute top-[18%] left-[15%] ">
-                <p className="text-white font-font_Inter font-semibold text-[32px] text-center">
+        <div className="bg-transparent px-2 py-4 mx-2 relative lg:absolute lg:top-[18%] lg:left-[7%] lg:px-4 lg:py-8 lg:mx-5">
+          <div className="bg-white w-full max-w-[426px] h-auto rounded-xl flex flex-col justify-center items-center mx-auto py-4 md:h-[286px]">
+            <div className="w-[50px] h-[50px] bg-gray-200 rounded-full relative md:w-[70px] md:h-[70px]">
+              <div className="w-[32px] h-[32px] rounded-full bg-black absolute top-[18%] left-[15%] flex items-center justify-center md:w-[45px] md:h-[45px]">
+                <p className="text-white font-font_Inter font-semibold text-[20px] text-center md:text-[32px]">
                   1
                 </p>
               </div>
             </div>
-            <p className="font-lato font-semibold text-[24px] mt-4">
+            <p className="font-lato font-semibold text-[18px] mt-2 md:text-[24px] md:mt-4">
               LOREM IPSUM
             </p>
-            <p className="font-main font-normal text-[16px] leading-[27px] ml-6 mr-6 w-[340px] text-center">
+            <p className="font-main font-normal text-[14px] leading-[22px] mx-2 w-full text-center md:text-[16px] md:leading-[27px] md:mx-6 md:w-[340px]">
               There are many variations of passages of Lorem Ipsum available,
               but the majority have suffered alteration
             </p>
           </div>
         </div>
 
-          {/* arrow svg created by arrow svg generator and position thik korsi className diea in svg */}
-
+        {/* arrow svg */}
         <svg
           xmlns="http://www.w3.org/2000/svg"
           version="1.1"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          xmlns:svgjs="http://svgjs.dev/svgjs"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          xmlnsSvgjs="http://svgjs.dev/svgjs"
           viewBox="0 0 800 800"
-          className="absolute  top-[17%] left-[32%] w-[500px] h-[400px]"
+          className="hidden lg:block absolute top-[7%] left-[31%] w-[500px] h-[600px]"
         >
           <g
-            stroke-width="10"
+            strokeWidth="10"
             stroke="hsl(231, 100%, 64%)"
             fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-dasharray="31.5 50"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeDasharray="31.5 50"
           >
             <path
               d="M250 250Q450 350 550 550 "
-              marker-end="url(#SvgjsMarker1006)"
+              markerEnd="url(#SvgjsMarker1006)"
             ></path>
           </g>
           <defs>
@@ -81,19 +80,19 @@ const Works = () => {
         </svg>
 
         {/* card_2 */}
-        <div className="bg-transparent px-4 py-8 mr-5  ml-5 absolute top-[34%] right-[7%]">
-          <div className="bg-white w-[426px] h-[286px] rounded-xl flex flex-wrap flex-col justify-center items-center">
-            <div className="w-[70px] h-[70px] bg-gray-200 rounded-full relative">
-              <div className="w-[45px] h-[45px] rounded-full bg-black absolute top-[18%] left-[15%] ">
-                <p className="text-white font-font_Inter font-semibold text-[32px] text-center">
+        <div className="bg-transparent px-2 py-4 mx-2 relative lg:absolute lg:top-[34%] lg:right-[7%] lg:px-4 lg:py-8 lg:mx-5">
+          <div className="bg-white w-full max-w-[426px] h-auto rounded-xl flex flex-col justify-center items-center mx-auto py-4 md:h-[286px]">
+            <div className="w-[50px] h-[50px] bg-gray-200 rounded-full relative md:w-[70px] md:h-[70px]">
+              <div className="w-[32px] h-[32px] rounded-full bg-black absolute top-[18%] left-[15%] flex items-center justify-center md:w-[45px] md:h-[45px]">
+                <p className="text-white font-font_Inter font-semibold text-[20px] text-center md:text-[32px]">
                   2
                 </p>
               </div>
             </div>
-            <p className="font-lato font-semibold text-[24px] mt-4">
+            <p className="font-lato font-semibold text-[18px] mt-2 md:text-[24px] md:mt-4">
               LOREM IPSUM
             </p>
-            <p className="font-main font-normal text-[16px] leading-[27px] ml-6 mr-6 w-[340px] text-center">
+            <p className="font-main font-normal text-[14px] leading-[22px] mx-2 w-full text-center md:text-[16px] md:leading-[27px] md:mx-6 md:w-[340px]">
               There are many variations of passages of Lorem Ipsum available,
               but the majority have suffered alteration
             </p>
@@ -103,23 +102,23 @@ const Works = () => {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           version="1.1"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          xmlns:svgjs="http://svgjs.dev/svgjs"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          xmlnsSvgjs="http://svgjs.dev/svgjs"
           viewBox="0 0 800 800"
-          className="absolute top-[47%] left-[29%] w-[500px] h-[400px] "
+          className="hidden lg:block absolute top-[37%] left-[30%] w-[500px] h-[600px]"
         >
           <g
-            stroke-width="10"
+            strokeWidth="10"
             stroke="hsl(231, 100%, 64%)"
             fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-dasharray="31.5 50"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeDasharray="31.5 50"
             transform="rotate(95, 400, 400)"
           >
             <path
               d="M250 250Q450 350 550 550 "
-              marker-end="url(#SvgjsMarker1031)"
+              markerEnd="url(#SvgjsMarker1031)"
             ></path>
           </g>
           <defs>
@@ -141,19 +140,19 @@ const Works = () => {
         </svg>
 
         {/* card_3 */}
-        <div className="bg-transparent px-4 py-8 mr-5  ml-5 absolute top-[55%] left-[7%] ">
-          <div className="bg-white w-[426px] h-[286px] rounded-xl flex flex-wrap flex-col justify-center items-center">
-            <div className="w-[70px] h-[70px] bg-gray-200 rounded-full relative">
-              <div className="w-[45px] h-[45px] rounded-full bg-black absolute top-[18%] left-[15%] ">
-                <p className="text-white font-font_Inter font-semibold text-[32px] text-center">
+        <div className="bg-transparent px-2 py-4 mx-2 relative lg:absolute lg:top-[55%] lg:left-[7%] lg:px-4 lg:py-8 lg:mx-5">
+          <div className="bg-white w-full max-w-[426px] h-auto rounded-xl flex flex-col justify-center items-center mx-auto py-4 md:h-[286px]">
+            <div className="w-[50px] h-[50px] bg-gray-200 rounded-full relative md:w-[70px] md:h-[70px]">
+              <div className="w-[32px] h-[32px] rounded-full bg-black absolute top-[18%] left-[15%] flex items-center justify-center md:w-[45px] md:h-[45px]">
+                <p className="text-white font-font_Inter font-semibold text-[20px] text-center md:text-[32px]">
                   3
                 </p>
               </div>
             </div>
-            <p className="font-lato font-semibold text-[24px] mt-4">
+            <p className="font-lato font-semibold text-[18px] mt-2 md:text-[24px] md:mt-4">
               LOREM IPSUM
             </p>
-            <p className="font-main font-normal text-[16px] leading-[27px] ml-6 mr-6 w-[340px] text-center">
+            <p className="font-main font-normal text-[14px] leading-[22px] mx-2 w-full text-center md:text-[16px] md:leading-[27px] md:mx-6 md:w-[340px]">
               There are many variations of passages of Lorem Ipsum available,
               but the majority have suffered alteration
             </p>
@@ -163,23 +162,23 @@ const Works = () => {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           version="1.1"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          xmlns:svgjs="http://svgjs.dev/svgjs"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          xmlnsSvgjs="http://svgjs.dev/svgjs"
           viewBox="0 0 800 800"
-          className="absolute top-[60%] left-[29%] w-[500px] h-[400px]"
+          className="hidden lg:block absolute top-[65%] left-[29%] w-[500px] h-[500px]"
         >
           <g
-            stroke-width="10"
+            strokeWidth="10"
             stroke="hsl(231, 100%, 64%)"
             fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-dasharray="31.5 50"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeDasharray="31.5 50"
             transform="matrix(1,0,0,1,44,-60)"
           >
             <path
               d="M250 250Q450 350 550 550 "
-              marker-end="url(#SvgjsMarker1186)"
+              markerEnd="url(#SvgjsMarker1186)"
             ></path>
           </g>
           <defs>
@@ -201,19 +200,19 @@ const Works = () => {
         </svg>
 
         {/* card_4 */}
-        <div className="bg-transparent px-4 py-8 mr-5  ml-5 absolute top-[75%] right-[6%]">
-          <div className="bg-white w-[426px] h-[286px] rounded-xl flex flex-wrap flex-col justify-center items-center">
-            <div className="w-[70px] h-[70px] bg-gray-200 rounded-full relative">
-              <div className="w-[45px] h-[45px] rounded-full bg-black absolute top-[18%] left-[15%] ">
-                <p className="text-white font-font_Inter font-semibold text-[32px] text-center">
+        <div className="bg-transparent px-2 py-4 mx-2 relative lg:absolute lg:top-[75%] lg:right-[6%] lg:px-4 lg:py-8 lg:mx-5">
+          <div className="bg-white mb-4 w-full max-w-[426px] h-auto rounded-xl flex flex-col justify-center items-center mx-auto py-4 md:h-[286px]">
+            <div className="w-[50px] h-[50px] bg-gray-200  rounded-full relative md:w-[70px] md:h-[70px]">
+              <div className="w-[32px] h-[32px] rounded-full bg-black absolute top-[18%] left-[15%] flex items-center justify-center md:w-[45px] md:h-[45px]">
+                <p className="text-white font-font_Inter font-semibold text-[20px] text-center md:text-[32px]">
                   4
                 </p>
               </div>
             </div>
-            <p className="font-lato font-semibold text-[24px] mt-4">
+            <p className="font-lato font-semibold text-[18px] mt-2 md:text-[24px] md:mt-4">
               LOREM IPSUM
             </p>
-            <p className="font-main font-normal text-[16px] leading-[27px] ml-6 mr-6 w-[340px] text-center">
+            <p className="font-main font-normal text-[14px] leading-[22px] mx-2 w-full text-center md:text-[16px] md:leading-[27px] md:mx-6 md:w-[340px]">
               There are many variations of passages of Lorem Ipsum available,
               but the majority have suffered alteration
             </p>
