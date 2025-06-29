a137915e8900631bd540850fb8b9763ae75e6401
azlinaaylin <azlinaarabi@gmail.com>
2025-06-29 13:49:43 +0600

    your home part code changes for responsive

--- src/components/YourHome.jsx
+++ src/components/YourHome.jsx
@@ -4,7 +4,7 @@ import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import img_1 from "../assets/images/YourHome.png";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
-import Button from "../commoncomponents/Button"
+import Button from "../commoncomponents/Button";
 const YourHome = () => {
   const item = [
     {
@@ -28,7 +28,7 @@ const YourHome = () => {
       id: 5,
       img: img_1,
     },
-     {
+    {
       id: 6,
       img: img_1,
     },
@@ -43,6 +43,29 @@ const YourHome = () => {
     pauseOnHover: true,
     nextArrow: <SampleNextArrow />,
     prevArrow: <SamplePrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1280, // xl
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 1024, // lg
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 640, // sm
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
   //react slick slider customArrows
   function SampleNextArrow(props) {
@@ -94,33 +117,33 @@ const YourHome = () => {
   return (
     <div>
       <section className="px-2 py-4 bg-purple">
-        <div className="px-2 py-4 ml-5 mr-5 bg-transparent">
-            <p className="font-lato font-bold text-[42px] leading-[32px]  text-center mb-8">For Your Home</p>
-          
-            {/* slick slider */}
-
-            <div className="slider-container">
-              <Slider {...settings}>
-                {item.map((data) => (
-                  <div className="p-6 ">
-                    <div className="bg-transparent shadow-lg w-[306px] h-[290px] rounded-lg flex justify-center items-center cursor-pointer relative">
-                      <img
-                        src={data.img}
-                        className="w-full h-full  object-contain "
+        <div className="px-2 py-4 mx-auto max-w-7xl bg-transparent">
+          <p className="font-lato font-bold text-[28px] md:text-[36px] lg:text-[42px] leading-[32px] text-center mb-8">
+            For Your Home
+          </p>
+          {/* slick slider */}
+          <div className="slider-container">
+            <Slider {...settings}>
+              {item.map((data, idx) => (
+                <div className="p-2 sm:p-4 md:p-6" key={idx}>
+                  <div className="bg-transparent  w-full max-w-[306px] h-[200px] sm:h-[250px] md:h-[290px] rounded-lg flex justify-center items-center cursor-pointer relative mx-auto">
+                    <img
+                      src={data.img}
+                      className="w-full h-full object-contain rounded-lg"
+                      alt="Your Home"
+                    />
+                    <div className="px-1 py-1 bg-transparent absolute bottom-[10%] left-1/2 -translate-x-1/2">
+                      <Button
+                        btnDesign="px-2 py-1 sm:px-4 sm:py-2 bg-gray-300 text-white rounded-lg font-lato font-normal text-[12px] sm:text-[14px] md:text-[16px] leading-[24px]"
+                        btnContent={"Plumbing and Sanitary Services"}
                       />
-                      <div className="px-1 py-1 bg-transparent absolute bottom-[10%]">
-
-                        <Button btnDesign="px-4 py-2 bg-gray-300 text-white rounded-lg font-lato font-normal text-[16px] leading-[24px]" btnContent={"Plumbing and Sanitary Services"}/>
-                        
-                      </div>
                     </div>
                   </div>
-                ))}
-              </Slider>
-
-              {/* slick slider */}
-           
+                </div>
+              ))}
+            </Slider>
           </div>
+          {/* slick slider */}
         </div>
       </section>
     </div>
